d8f047ab82cb054260c3ae8aa542a28ad5b4b09d
eliboss <eacrouther@gmail.com>
2023-06-25 12:20:56 -0700

    add host to app run

--- twilio_manager/twilio_messanger.py
+++ twilio_manager/twilio_messanger.py
@@ -35,7 +35,7 @@ def incoming_sms():
     return str(resp)
 
 def run_server():
-    app.run(port=5000,debug=False)
+    app.run(host='0.0.0.0',port=5000)
 
 if __name__ == "__main__":
     run_server()
