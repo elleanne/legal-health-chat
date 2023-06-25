90e1e5db9d3ff2a2f5fba9bf9ecd6a101517e6f5
eliboss <eacrouther@gmail.com>
2023-06-25 13:00:19 -0700

    add gunicorn

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-import twilio_manager.twilio_messanger 
+from twilio_manager.twilio_messanger import run_server
 
-# def main():
-#     run_server()
+def main():
+    run_server()
     
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
